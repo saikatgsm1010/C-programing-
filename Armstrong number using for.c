6a03e4b6bd2d160ff1c041594499eb3f376a44cf
Gourav dey <52992304+Gourav9905@users.noreply.github.com>
2020-05-01 10:48:46 +0530

    1st upload

---
+++ Armstrong number using for.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int num,i,x,s=0,copy;
+ copy=num;
+ printf("Enter a number");
+ scanf("%d",&num);
+ for(i=num;i>0;x=i/10)
+ {
+   x=num%10;
+   s=s+x*x*x;
+   }
+    if(copy==s)
+    {
+      printf("armstong number");
+      }
+      else
+      {
+       printf("not armstorng");
+       }
+       getch();
+ }
